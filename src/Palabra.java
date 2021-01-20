eb576f88aded9776b8ce9809fa03572c6eac4e5e
kaizo13 <alexcampo13@outlook.com>
2021-01-20 11:07:23 +0100

    Ejercicio simon de palabras

---
+++ src/Palabra.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Palabra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <String> palabras= new ArrayList <>(Arrays.asList("Boligrafo","Carpeta","Estuche","Rotulador","Goma"));
+		ArrayList <String> dichas=new ArrayList<>();	
+		Scanner dato=new Scanner(System.in);
+		Boolean cierto=true;
+		int aciertos =0;
+		while(dichas.size()<palabras.size() && cierto) {
+		
+			String palabra=dimePalabra(palabras,dichas);
+			dichas.add(palabra);
+			System.out.println(palabra);
+			String entrada=dato.nextLine();
+			
+			
+			String [] lista=entrada.split(" ");
+			
+			for(int i=0;i<dichas.size();i++) {
+				if(!lista[i].equals(dichas.get(i))) {
+					cierto=false;
+				}
+			}
+			if(cierto) {
+				aciertos++;
+				System.out.println(dichas.size());
+			}else {
+				System.out.println("Error!!!!!!!!");
+			}
+		
+			
+		}
+		
+		System.out.println("Has acertado " + aciertos + " de " + palabras.size());
+	}
+	
+	public static String dimePalabra(ArrayList <String> palabras, ArrayList <String> dichas) {
+		Random rand=new Random();
+		int index=rand.nextInt(palabras.size());
+		Boolean existe=false;
+		String palabra=palabras.get(index);
+		
+		for(int k=0;k<dichas.size();k++) {
+			
+			if(palabra.equals(dichas.get(k))) {
+				existe=true;
+			}
+		}
+		
+		if(existe) {
+			palabra=dimePalabra(palabras, dichas);
+		}
+				
+		return palabra;
+	}
+
+}
